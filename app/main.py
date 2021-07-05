e68aca6d9d5fb215b3e73fd4552a530ae4d7f3c1
alessonalves <alesson.alves@me.com>
2021-07-05 19:45:09 +0000

    exercicio python/selenium

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-print("test")
+from selenium.webdriver import Firefox
+from time import sleep
+
+url = "https://curso-python-selenium.netlify.app/aula_03.html"
+navegador = Firefox()
+navegador.get (url)
+sleep (20)
+
+a = navegador.find.element_by_tag_name("a")
+p = navegador.find.element_by_tag_name("p")
+
+print (f"texto de a: {a.text}")
+print (f"texto de p: {b.text}")
